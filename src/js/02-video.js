9b3cb5227e6b0120ee696b2e8193fa69e8d03b14
Vorzelskiy <evgeniysashko@gmail.com>
2023-05-11 19:57:39 +0300

    creat task 02,03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerOptions = {
+  url: 'https://player.vimeo.com/video/236203659',
+  width: 640,
+  height: 360
+};
+
+const videoPlayer = document.getElementById('video-container');
+
+const player = new Player(videoPlayer, playerOptions);
+
+// Отримуємо поточний час відтворення та зберігаємо його у локальному сховищі
+function saveCurrentTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+// Отримуємо збережений час відтворення з локального сховища
+function getSavedCurrentTime() {
+  return localStorage.getItem('videoplayer-current-time');
+}
+
+// Встановлюємо час відтворення плеєра
+function setCurrentTime(time) {
+  player.setCurrentTime(time);
+}
+
+// Підписуємося на подію timeupdate, використовуючи lodash.throttle для обмеження частоти оновлення часу
+player.on('timeupdate', throttle(function(data) {
+  const currentTime = data.seconds;
+  saveCurrentTime(currentTime);
+}, 1000));
+
+// Відновлюємо відтворення з збереженого часу
+const savedCurrentTime = getSavedCurrentTime();
+if (savedCurrentTime !== null) {
+  setCurrentTime(savedCurrentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+// Отримання елементів форми
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+// Ключ для збереження у локальне сховище
+const storageKey = 'feedback-form-state';
+
+// Функція, яка зберігає значення полів у локальне сховище
+const saveFormState = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formState));
+};
+
+// Функція, яка заповнює поля форми зі значеннями з локального сховища
+const loadFormState = () => {
+  const savedFormState = localStorage.getItem(storageKey);
+  if (savedFormState) {
+    const formState = JSON.parse(savedFormState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+// Функція, яка очищує локальне сховище та поля форми
+const clearFormState = () => {
+  localStorage.removeItem(storageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+// Функція, яка виводить у консоль об'єкт зі значеннями полів форми
+const submitForm = (event) => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState);
+
+  clearFormState();
+};
+
+// Додавання обробників подій до форми
+form.addEventListener('input', throttle(saveFormState, 500));
+window.addEventListener('load', loadFormState);
+form.addEventListener('submit', submitForm);
